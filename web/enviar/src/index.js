52f5ab215d3ff648196923b321854c5b0c67c9aa
imdeepmind <infinityatme@gmail.com>
2019-02-21 21:50:54 +0530

    added bootstrap and a theme

--- web/enviar/src/index.js
+++ web/enviar/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import './assets/css/lux/bootstrap.min.css';
+import './assets/css/lux/_variables.scss';
+import './assets/css/lux/_bootswatch.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
