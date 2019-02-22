2098ed5f8d6616042e8fbb069297a430bf1322f9
imdeepmind <infinityatme@gmail.com>
2019-02-22 17:59:04 +0530

    added a theme in bootstrap

--- web/enviar/src/App.js
+++ web/enviar/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {
-  Button
+  Button,
+  Input
 } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Input />
         <Button color="primary">Hello</Button>
       </div>
     );
--- web/enviar/src/index.js
+++ web/enviar/src/index.js
@@ -5,10 +5,7 @@ import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-import './assets/css/lux/bootstrap.min.css';
-import './assets/css/lux/_variables.scss';
-import './assets/css/lux/_bootswatch.scss';
-
+import './assets/css/theme.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
