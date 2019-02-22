99fbefc95b98ba8355dcec6310aefbfd54609e8a
imdeepmind <infinityatme@gmail.com>
2019-02-22 20:58:28 +0530

    added a simple page

--- web/enviar/src/App.js
+++ web/enviar/src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import {
+  Container,
+  Row,
+  Col,
+  Form,
   Button,
-  Input
+  Input,
 } from 'reactstrap';
+
+import TopNav from './containers/topNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Something is awesome</h1>
-        <Input />
-        <Button color="primary">Hello</Button>
+        <TopNav />
+        <Container className="mt-4">
+          <Row>
+            <Col xs="12">
+              <Form>
+                <div className="d-flex">
+                  <Input placeholder="Search..." />
+                  <Button color="primary" >Search</Button>
+                </div>
+              </Form>
+            </Col>
+            <Col xs="12" lg="8" className="mt-4">
+              <h1>FEED</h1>
+            </Col>
+            <Col xs="12" lg="4" className="mt-4">
+              <h1>Additional Information</h1>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
---
+++ web/enviar/src/containers/topNav/index.js
@@ -0,0 +1,55 @@
+import React, { Component, Fragment } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+
+class TopNav extends Component{
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render(){
+        return (
+            <div>
+                <Navbar color="primary" dark expand="md">
+                    <NavbarBrand href="/" title="home">enviar</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink title="notifications" href="/components/"><i class="fas fa-bell"></i></NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink title="messages" href="https://github.com/reactstrap/reactstrap"><i class="fas fa-comments"></i></NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink title="profile" href="https://github.com/reactstrap/reactstrap"><i class="fas fa-user-alt"></i></NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink title="log out" href="https://github.com/reactstrap/reactstrap"><i class="fas fa-sign-out-alt"></i></NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default TopNav;
