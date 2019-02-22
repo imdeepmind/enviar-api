800cda7c01901d6ea06eddedebbaf18e192288a0
imdeepmind <infinityatme@gmail.com>
2019-02-22 19:46:31 +0530

    updated the theme

--- web/enviar/src/App.js
+++ web/enviar/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Something is awesome</h1>
         <Input />
         <Button color="primary">Hello</Button>
       </div>
