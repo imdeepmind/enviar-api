7b91417f711d3b2d59323f5e7416333b7fa613c4
imdeepmind <infinityatme@gmail.com>
2019-02-24 22:26:21 +0530

    added login page and updtae the landing page

--- web/enviar/src/components/higher/landing/index.js
+++ web/enviar/src/components/higher/landing/index.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from 'react';
+import { Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const Landing = props => {
     return (
         <Fragment>
             <div className="class-landing d-flex align-items-start justify-content-center flex-column bg-primary p-5">
-                <h1 className="display-3 text-white">enviar</h1>
-                <h2 className="text-light">a simple social network platform for sharing your photos</h2>
-                <p className="text-light"><Link to="/login" className="text-light">login</Link> or <Link to="/register" className="text-light">register</Link></p>
+                <Row>
+                    <Col xs="12" lg="6">
+                        <h1 className="display-3 text-white">enviar</h1>
+                        <h2 className="text-light">a simple social network platform for sharing your moments</h2>
+                        <p className="text-light"><Link to="/login" className="text-light">login</Link> or <Link to="/register" className="text-light">register</Link></p>
+                    </Col>
+                </Row>
             </div>
         </Fragment>
     )
--- web/enviar/src/routes/login/index.js
+++ web/enviar/src/routes/login/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { Card, CardBody, CardTitle, Form, Input, Button } from 'reactstrap';
 
 import Landing from '../../components/higher/landing';
 
@@ -7,6 +8,20 @@ class Login extends Component{
         return (
             <Fragment>
                 <Landing />
+                <div className="w-100 d-flex justify-content-center align-items-center pt-5 pb-5">
+                    <Card>
+                        <CardBody>
+                            <CardTitle className="text-center">
+                                login
+                            </CardTitle>
+                            <Form>
+                                <Input placeholder="username..." required />
+                                <Input type="password" placeholder="password..." required />
+                                <Button type="submit" color="primary" className="w-100">login</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </div>
             </Fragment>
         )
     }
