737aaf3b2d76e56e7584c65ff76921cc7620f281
imdeepmind <infinityatme@gmail.com>
2019-04-14 22:53:01 +0530

    added more user services

--- api/app/services/user-service.js
+++ api/app/services/user-service.js
@@ -1,20 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 import Q from 'q';
 
 import users from '../models/users';
 import logger from '../utils/logger';
 import processInput from '../utils/process-input';
 
-export const findByID = id => {
+export const findByID = (id, fields) => {
     const deferred = Q.defer();
     const findQuery = {
         "_id" : {$eq: mongoose.Types.ObjectId(id)}
     }
 
-    users.findById(findQuery, (err, doc) => {
+    users.findById(findQuery, fields, (err, doc) => {
         if (err) {
             deferred.reject(err);
-            logger.debug('Database error', err);
+            logger.info('Database error', err);
         } else if (doc) {
             deferred.resolve(doc);
         } else {
@@ -25,23 +25,16 @@ export const findByID = id => {
     return deferred.promise;
 }
 
-export const findByUsername = username => {
+export const findByUsername = (username, fields) => {
     const deferred = Q.defer();
-
-    username = processInput(username, 'username');
-    if (!username){
-        deferred.reject('Invalid username');
-    }
-        
-
     const findQuery = {
         "username" : {$eq: processInput(username, 'username')}
     }
 
-    users.findOne(findQuery, (err, doc) => {
+    users.findOne(findQuery, fields, (err, doc) => {
         if (err) {
             deferred.reject(err);
-            logger.debug('Database error', err);
+            logger.info('Database error', err);
         } else if (doc) {
             deferred.resolve(doc);
         } else {
@@ -52,6 +45,66 @@ export const findByUsername = username => {
     return deferred.promise;
 }
 
-// export const insert = () => {
-//     const deferred = Q.defer();
-// }
+export const insert = dt => {
+    const deferred = Q.defer();
+
+    dt._id = new mongoose.Types.ObjectId();
+
+    const u = users(dt);
+
+    findByUsername(dt.username)
+    .then(resp => {
+        logger.info(`Username ${dt.username} already exists`);
+        deferred.reject('Username already exists'); 
+    })
+    .catch(_ => {
+        u.save((err, doc) => {
+            if (err){
+                deferred.reject(err);
+                logger.info('Database error', err);
+            } else if (doc) {
+                deferred.resolve(doc);
+            }
+        })
+    })
+
+    return deferred.promise;
+}
+
+export const updateByID = (dt, id) => {
+    const deferred = Q.defer();
+    
+    const findQuery = {
+        '_id' : {$eq: mongoose.Types.ObjectId(id)}
+    }
+
+    users.findOneAndUpdate(findQuery, dt, (err, doc) => {
+        if (err){
+            deferred.reject(err);
+            logger.info('Database error', err);
+        } else if (doc) {
+            deferred.resolve(doc);
+        }
+    })
+
+    return deferred.promise;
+}
+
+export const updateByUsername = (dt, id) => {
+    const deferred = Q.defer();
+    
+    const findQuery = {
+        'username' : {$eq: username}
+    }
+
+    users.findOneAndUpdate(findQuery, dt, (err, doc) => {
+        if (err){
+            deferred.reject(err);
+            logger.info('Database error', err);
+        } else if (doc) {
+            deferred.resolve(doc);
+        }
+    })
+
+    return deferred.promise;
+}
--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import logger from './app/utils/logger';
 import connect from './app/utils/db'; 
 
+import { insert } from './app/services/user-service';
+
 const app = express();
 const port = process.env.PORT || 4999;
 const webPass = process.env.WEBPASS || ['*'];
@@ -42,6 +44,28 @@ app.put('*', function (req, res, next) {
 	err.status = 404;
 	next(err);
 });
+
+insert({
+    "username" : "imdeepmind",
+    "password" : "12345",
+    "name" : "Abhishek Chatterjee",
+    "email" : "infinityatme@gmail.com",
+    "city" : "Guwahati",
+    "state" : "Assam",
+    "country" : "India",
+    "gender" : "m",
+    "dob" : "1997-01-17",
+    "avatar" : "",
+    "status" : "Sample status",
+    "bio" : "Sample bio"
+})
+.then(resp => {
+    logger.info('resp');
+})
+.catch(err => {
+    logger.info(err);
+})
+
 
 app.listen(port, () => {
     logger.info(`The API is running at port ${port}`);
