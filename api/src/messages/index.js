67706678e3c40218520917cbe76fa04fed70a1ab
imdeepmind <infinityatme@gmail.com>
2019-04-14 15:21:13 +0530

    added new project structure

--- api/src/messages/index.js
+++
@@ -1,7 +0,0 @@
-export const dbError = "Something went wrong with the database server!";
-export const mainServerError = "Something went wrong with the main server!";
-export const usernameExists = "Username or email already exists!";
-export const usernameError = "Username does not exists!";
-export const passwordError = "Wrong password!";
-export const validationError = "Validation error!"
-export const unauthorised = "Unauthorised";
