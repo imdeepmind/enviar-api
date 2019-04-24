ba761b96ff5e7d7e483721b6779a47fcc3f60896
imdeepmind <infinityatme@gmail.com>
2019-04-24 20:32:53 +0530

    added incomplete chat contreoller

--- api/app/controller/chats.js
+++ api/app/controller/chats.js
@@ -1,6 +1,9 @@
 import xss from 'xss';
+import mongoose from 'mongoose';
 
 import chatModel from '../models/chats';
+import userModel from '../models/users';
+
 import logger from '../utils/logger';
 import messages from '../messages';
 
@@ -33,15 +36,61 @@ export const getChats = (req, res) => {
             return res.status(200).json(doc);
         }
     })
-
 }
 
 export const getChat = (req, res) => {
+    const findQuery = {
+        $or: [
+            {'author' : {$eq: me}},
+            {'to' : {$eq: me}}
+        ],
+        _id: mongoose.Schema.Types.ObjectId(req.params.id)
+    }
 
+    const selectedField = {
+        _id: 1, author: 1, to: 1, message: 1, createdAt: 1
+    }
+
+    chatModel.find(findQuery, selectedField, (err, doc) => {
+        if (err) {
+            logger.debug('Validation didn\'t succeed');
+            return res.boom.badRequest(messages['m400.2'], err);
+        } else {
+            logger.debug('Returning some chats');
+            return res.status(200).json(doc);
+        }
+    })
 }
 
 export const postChat = (req, res) => {
+    req.check('status', 'Invalid status').isString().isLength({min:4, max:255});
 
+    const errors = req.validationErrors();
+    if (errors) {
+        logger.debug('Validation didn\'t succeed');
+        return res.boom.badRequest(messages['m400.2'], errors);
+    }
+
+    const findQuery = {
+        'username' : {$eq: xss(req.authData.username)}
+    }
+
+    const selectedField = { _id: 1, blocked: 1 }
+
+    userModel.findOne(findQuery, selectedField, (err, doc) => {
+        if (err) {
+            logger.debug('Validation didn\'t succeed');
+            return res.boom.badRequest(messages['m400.2'], err);
+        } else {
+            if (doc.blocked.includes(req.params.username)){
+
+            } else {
+                const findQuery2 = {
+                    
+                }
+            }
+        }
+    })
 }
 
 export const deleteChat = (req, res) => {
