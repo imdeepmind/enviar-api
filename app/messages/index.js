568a0c95bebb5bc3bc367cf32a0e8743b4d97a2a
imdeepmind <infinityatme@gmail.com>
2019-06-05 16:53:40 +0530

    edited the user exists mesage

--- app/messages/index.js
+++ app/messages/index.js
@@ -2,7 +2,7 @@ export default {
     'm201.0' : "User created successfully",
     'm201.1' : "User exists",
 
-    'm400.0' : "Username or password already exists",
+    'm400.0' : "Username already exists",
     'm400.1' : "Email already exists",
     'm400.2' : "Validation didn't succeed",
     'm400.3' : "You are blocked by the user",
